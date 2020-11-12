4a809483ff5674a6b6b0531fbc59397dbb091a32
thibaut <thibaut.morales@epitech.eu>
2020-11-12 17:25:22 +0100

    fix eslint

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = require('./src/router');
-const path = require('path'); 
-require('dotenv').config({ path: `./.env.${process.env.NODE_ENV}/`});
+
+require('dotenv').config({ path: `./.env.${process.env.NODE_ENV}/` });
 
 const app = express();
 const port = process.env.PORT;
